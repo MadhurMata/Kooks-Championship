cab76b47d949041fd5804032378a5ee9659b9172
Madhur Suria Mata Garcia <madhurmata@Madhurs-MacBook-Pro.local>
2019-01-20 16:31:04 +0100

    8 enemies in screen

--- src/enemy.js
+++ src/enemy.js
@@ -1,22 +1,34 @@
 "use strict"
 
 
-function Enemy(canvas){
-  this.x =  100;
-  this.y = 100; 
+function Enemy(canvas, y, speedX, speedY){
+  this.y = y;
   this.size = 20;
-  this.speed = 2;
+  this.x =  canvas.width-(this.size * 5);
+  this.speedX = speedX;
+  this.speedY = speedY;
+  this.canvas = canvas;
+  this.directionX = 1;
+  this.directionY = 1;
   this.ctx = canvas.getContext('2d');
-
 }
 
 Enemy.prototype.draw = function(){
   this.ctx.fillRect(this.x, this.y, this.size, this.size);
-
 };
 
 Enemy.prototype.update = function (){
-  this.x -= this.speed;
+  console.log(this.y);
+  this.x -= this.directionX * this.speedX;
+  this.y += this.directionY * this.speedY;
+  
+  if ((this.y <= 0) || (this.y >= this.canvas.height - this.size)) {
+    this.directionY *= -1;
+}
+if ((this.x <= 0) || (this.x >= this.canvas.width - this.size)) {
+  this.directionX *= -1;
+}
+  
 
 };
 
--- src/game.js
+++ src/game.js
@@ -3,38 +3,54 @@
 function Game(canvas) {
   this.ctx = canvas.getContext("2d");
   this.player = new Player(canvas);
-  this.enemy = new Enemy(canvas);
   this.enemies = [];
   this.animation;
 
-  this._clearCanvas = function() {
-    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  this._drawCanvas = function() {
-        this.player.draw();
-        this.enemy.draw();
-        
-    }
-
-  this._updateGame = function() {
-      this.player.update();
-
-      this.enemy.update();
-      
-  }
 };
 
-  
+Game.prototype.clearCanvas = function() {
+  this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+Game.prototype.drawCanvas = function(){
+      this.player.draw();
+      this.enemies.forEach(function(enemy){
+        enemy.draw();
+      });      
+}
+
+Game.prototype.updateGame = function() {
+    this.player.update();
+
+    if (Math.random()> 0.98 && this.enemies.length < 8){
+      this.createEnemies();
+    };
+      this.enemies.forEach(function(enemy){
+        enemy.update();
+      }.bind(this));
+    
+  };
+
+Game.prototype.createEnemies = function(){
+  var speedX = Math.round(Math.random() * 2);
+  var y = Math.round(Math.random() * canvas.height);
+  var direction = Math.round(Math.random());
+  var speedY = Math.round(Math.random() * 2);
+  if(direction === 0){
+    speedY = Math.round(Math.random() * 2)*(-1);
+  }
+  this.enemies.push(new Enemy(canvas, y, speedX, speedY));
+}
+
 
 Game.prototype.start = function(){
   function loop() {
 
-    this._updateGame();
+    this.updateGame();
 
-    this._clearCanvas();
+    this.clearCanvas();
 
-    this._drawCanvas();
+    this.drawCanvas();
 
     this.animation = window.requestAnimationFrame(loop.bind(this));
 
